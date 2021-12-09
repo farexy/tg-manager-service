0f248e08e072a0af8c96f4817c4d68b908efb81e
OleksandrTopchii <o.topchii@somniumgame.com>
2021-12-09 15:20:58 +0200

    fix config name

--- src/TG.Manager.Service/Config/TgConfigs.cs
+++ src/TG.Manager.Service/Config/TgConfigs.cs
@@ -3,7 +3,6 @@
     public static class TgConfigs
     {
         public const string RealtimeServerDeployment = "realtime-server-deployment"; 
-        //public const string BattleSettings = "api-battle-settings";
-        public const string BattleSettings = "bbb1";
+        public const string BattleSettings = "api-battle-settings";
     }
 }
